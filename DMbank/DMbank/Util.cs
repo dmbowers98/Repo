6a5805961c4483d2f47a9338521b02256b58a032
Dylan Bowers <DMB182@pitt.edu>
2018-01-23 20:27:15 -0500

    Added Util Class

---
+++ DMbank/DMbank/Util.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DMbank
+{
+    class Util
+    {
+        public static bool LogStatus(String statusText)
+        {
+            bool status = false;
+            //Statement starts false
+            try
+            {
+                System.IO.File.WriteAllText(Directory.GetCurrentDirectory() + "statuslog.txt", statusText);
+                status = true;
+                //Statement becomes true if the statuslog completes, if not it remains false
+            }
+            catch (Exception ex)
+            {
+
+            }
+            finally
+            {
+                
+            }
+            return status;
+            //returns either true or false depending on whether the statuslog completes
+        }
+    }
+}
